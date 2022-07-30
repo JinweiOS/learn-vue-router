0533955b1d21db055b635ead557d304bf85c6f3b
TuringCodeOne <1962732398@qq.com>
2022-07-30 16:24:08 +0800

    删除了readme.md

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <template slot="title"> 这是标题，字体超大&加粗显示 </template>
       <template v-slot:body> 内容还是丑红色 </template>
     </RedText> -->
-    <!-- <component v-for="(comps, index) in componentArr" :is="comps" :key="index"></component> -->
     <router-view />
   </div>
 </template>
