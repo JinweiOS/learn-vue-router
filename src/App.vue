9f86b0313a463408566781f4162f9fdc1efdd916
TuringCodeOne <1962732398@qq.com>
2022-07-30 16:25:23 +0800

    修改了app.vue文件”

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="app">
-    <!-- <RedText>
-      <template slot="title"> 这是标题，字体超大&加粗显示 </template>
-      <template v-slot:body> 内容还是丑红色 </template>
-    </RedText> -->
     <router-view />
   </div>
 </template>
